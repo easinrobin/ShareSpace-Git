3bb709d4d1e20ffa979ef110188c07bcbee2a97c
easinrobin <robinkhondokar@gmail.com>
2019-12-30 19:08:50 +0600

    Updated Rich text editor

--- ShareSpace/AdminPanel/App_Start/BundleConfig.cs
+++ ShareSpace/AdminPanel/App_Start/BundleConfig.cs
@@ -94,99 +94,15 @@ namespace AdminPanel
             bundles.Add(new ScriptBundle("~/bundles/select").Include(
                 "~/Scripts/ThirdPartyJs/bootstrap-select.min.js"));
 
-
-
-
+            
             //rich text
             bundles.Add(new ScriptBundle("~/bundles/rich-textJs-1").Include(
-                "~/Scripts/rich-textJs/codemirror.min.js"));
+                "~/Scripts/rich-textJs/trumbowyg.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/rich-textJs-2").Include(
-                "~/Scripts/rich-textJs/xml.min.js"));
+                "~/Scripts/rich-textJs/trumbowyg.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-3").Include(
-                "~/Scripts/rich-textJs/froala_editor.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-4").Include(
-                "~/Scripts/rich-textJs/align.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-5").Include(
-                "~/Scripts/rich-textJs/code_beautifier.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-6").Include(
-                "~/Scripts/rich-textJs/code_view.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-7").Include(
-                "~/Scripts/rich-textJs/colors.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-8").Include(
-                "~/Scripts/rich-textJs/draggable.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-9").Include(
-                "~/Scripts/rich-textJs/emoticons.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-10").Include(
-                "~/Scripts/rich-textJs/font_size.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-11").Include(
-                "~/Scripts/rich-textJs/font_family.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-12").Include(
-                "~/Scripts/rich-textJs/image.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-13").Include(
-                "~/Scripts/rich-textJs/file.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-14").Include(
-                "~/Scripts/rich-textJs/image_manager.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-15").Include(
-                "~/Scripts/rich-textJs/line_breaker.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-16").Include(
-                "~/Scripts/rich-textJs/link.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-17").Include(
-                "~/Scripts/rich-textJs/lists.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-18").Include(
-                "~/Scripts/rich-textJs/paragraph_format.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-19").Include(
-                "~/Scripts/rich-textJs/paragraph_style.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-20").Include(
-                "~/Scripts/rich-textJs/paragraph_style.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-21").Include(
-                "~/Scripts/rich-textJs/video.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-22").Include(
-                "~/Scripts/rich-textJs/table.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-23").Include(
-                "~/Scripts/rich-textJs/url.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-24").Include(
-                "~/Scripts/rich-textJs/entities.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-25").Include(
-                "~/Scripts/rich-textJs/char_counter.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-26").Include(
-                "~/Scripts/rich-textJs/inline_style.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-27").Include(
-                "~/Scripts/rich-textJs/save.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-28").Include(
-                "~/Scripts/rich-textJs/fullscreen.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-29").Include(
-                "~/Scripts/rich-textJs/quick_insert.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rich-textJs-30").Include(
-                "~/Scripts/rich-textJs/quote.min.js"));
+            
 
             #endregion
 
@@ -218,52 +134,11 @@ namespace AdminPanel
                 "~/Content/CustomCss/demo.css"));
 
             bundles.Add(new StyleBundle("~/Content/rich-text-1").Include(
-                "~/Content/rich-text/froala_editor.css"));
+                "~/Content/rich-text/trumbowyg.css"));
 
             bundles.Add(new StyleBundle("~/Content/rich-text-2").Include(
-                "~/Content/rich-text/froala_style.css"));
+                "~/Content/rich-text/trumbowyg.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/rich-text-3").Include(
-                "~/Content/rich-text/code_view.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-4").Include(
-                "~/Content/rich-text/colors.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-5").Include(
-                "~/Content/rich-text/emoticons.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-6").Include(
-                "~/Content/rich-text/image_manager.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-7").Include(
-                "~/Content/rich-text/image.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-8").Include(
-                "~/Content/rich-text/line_breaker.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-9").Include(
-                "~/Content/rich-text/table.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-10").Include(
-                "~/Content/rich-text/char_counter.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-11").Include(
-                "~/Content/rich-text/video.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-12").Include(
-                "~/Content/rich-text/fullscreen.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-13").Include(
-                "~/Content/rich-text/quick_insert.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-14").Include(
-                "~/Content/rich-text/file.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-15").Include(
-                "~/Content/rich-text/dark.css"));
-
-            bundles.Add(new StyleBundle("~/Content/rich-text-16").Include(
-                "~/Content/rich-text/codemirror.min.css"));
 
 
             //new
